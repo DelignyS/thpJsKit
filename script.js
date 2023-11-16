2abaab53522ccedd867912a4d306e43aaa07db4b
Sylvain.D <60609924+JerkStanton@users.noreply.github.com>
2023-11-16 09:03:46 +0100

    width fixed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 import { displayHelloWorld, askName } from './script_1.js';
 
 displayHelloWorld(); // Affiche "Bonjour, monde!" dans la console en appelant la fonction displayHelloWorld() du fichier script_1.js
-askName();
+askName();// Demande le prénom de l'utilisateur en appelant la fonction askName() du fichier script_1.js
